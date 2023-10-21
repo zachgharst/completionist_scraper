0ffd3474f1850a74518503e44d5a1accf7eb1337
Zach Gharst <zdgharst@gmail.com>
2023-10-21 14:36:57 -0500

    Add documentation and cleanup for sharing

--- main.go
+++ main.go
@@ -9,16 +9,33 @@ import (
 )
 
 func main() {
-    doc, err := htmlquery.LoadURL("https://completionist.me/steam/profile/76561197997651901")
+    // todo parse user id from args
+    printCompletionistData("76561198025094963")
+}
+
+func printCompletionistData(profile string) {
+    doc, err := htmlquery.LoadURL("https://completionist.me/steam/profile/")
     if err != nil {
+        // todo print to stderr and exit instead
         log.Fatalf("Couldn't load completionist profile: %s", err)
     }
 
+    // This xpath is a bit gross, but rather than work on improving it, I'd
+    // rather help implement an API on the website that returns the data
+    // instead.
     values, err := htmlquery.QueryAll(doc, "/html/body/div[2]/main/div[1]/div/div[2]/div/div[1]/div/div/div/dl/dt/span|/html/body/div[2]/main/div[1]/div/div[2]/div/div[1]/div/div/div/dl/dt/a/span")
     if err != nil {
+        // todo print to stderr and exit instead
         log.Fatalf("Couldn't find values: %s", err)
     }
 
+    // Originally, I tried to get the keys from a similar xpath, but it was
+    // hard to keep the xpath order consistent with the values. It's also
+    // possible to xpath into the "row" if you will (the key:value is a
+    // sequence that doesn't share a parent), but then I had a lot of special
+    // logic to parse lots of special rows that weren't consistent. Hard coding
+    // the keys seems to be the simplest solution to keep the script
+    // maintainable.
     keys := []string{
         "Achievements in Owned",
         "Daily Maximum",
@@ -53,9 +70,15 @@ func main() {
         "Games Restricted",
     }
 
+    // Should be a struct and should be marshalled to json. Dump to stdout,
+    // because saving to file means that we can't pipe. We could also return
+    // as a string and parameterize on the program args as a file/stdout, but
+    // let's not overengineer right now.
     fmt.Println("{")
     for i := range values {
         val := ""
+
+        // The first value is "Achievements" which has an extra span within it.
         if i == 0 {
             val = values[0].FirstChild.NextSibling.FirstChild.Data
         } else {
